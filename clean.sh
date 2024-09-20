58803378f22739734b57ad474fb2903b67ac98eb
Mati <mati@pop-os.fritz.box>
2024-09-20 17:19:32 +0200

    finish overview

---
+++ clean.sh
@@ -0,0 +1,14 @@
+find . -type f -name '*.aux' -delete
+find . -type f -name '*.fdb*' -delete
+find . -type f -name '*.fls' -delete
+find . -type f -name '*.log' -delete
+find . -type f -name '*.sync*' -delete
+find . -type f -name '*.toc' -delete
+find . -type f -name '*.snm' -delete
+find . -type f -name '*.nav' -delete
+find . -type f -name '*.out' -delete
+find . -type f -name '*.bak*' -delete
+find . -type f -name '*.vrb' -delete
+find . -type f -name '*.bcf' -delete
+find . -type f -name '*.bib' -delete
+find . -type f -name '*.xml' -delete
